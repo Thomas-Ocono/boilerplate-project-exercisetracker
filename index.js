87f86d29678832d6d484580339b4a7d32ecc4e02
Thomas O'Connor <thomas.Ocono@gmail.com>
2025-04-28 07:28:39 -0400

    messing around with mongo

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const dbURI = process.env.URI;
+const bodyParser = require("body-parser");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+mongoose
+  .connect(dbURI)
+  .then((result) => console.log("connected to db"))
+  .catch((err) => console.log(err));
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+const testSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: Number,
+});
 
+const testModel = mongoose.model("Test", testSchema);
 
+app.get("/api", (req, res) => {
+  const testInput = new testModel({
+    name: "test1",
+    age: 25,
+  });
+  testInput
+    .save()
+    .then((result) => {
+      res.send(result);
+      console.log("save success!");
+    })
+    .catch((err) => console.log(err));
+});
 
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+const listener = app.listen(process.env.PORT || 3500, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
