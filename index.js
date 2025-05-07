ed449335d180bba19b0bb3991358930cb8ccf603
Thomas O'Connor <thomas.Ocono@gmail.com>
2025-05-06 23:31:06 -0400

    can add exercises to db

--- index.js
+++ index.js
@@ -36,9 +36,71 @@ app.post("/api/users", (req, res) => {
   });
   user
     .save()
-    .then((result) => console.log("New User added!"))
+    .then((result) => {
+      console.log("New User added!");
+      res.send(user);
+    })
     .catch((err) => {
       console.log("Error saving user");
+      console.log(err);
+    });
+});
+
+//add exercises to user
+const exerciseSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+});
+const exerciseModel = mongoose.model("Exercises", exerciseSchema);
+
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const inputId = req.params._id;
+  const exerciseDescription = req.body.description;
+  const exerciseDuration = req.body.duration;
+  let exerciseDate = req.body.date;
+  if (!exerciseDate) {
+    let currentDate = new Date();
+    exerciseDate = currentDate.toDateString();
+  }
+
+  console.log(
+    "id: " +
+      inputId +
+      "| Description: " +
+      exerciseDescription +
+      "| Duration: " +
+      exerciseDuration +
+      "| Date: " +
+      exerciseDate
+  );
+  const newExercise = new exerciseModel({
+    id: inputId,
+    description: exerciseDescription,
+    duration: exerciseDuration,
+    date: exerciseDate,
+  });
+  newExercise
+    .save()
+    .then((result) => {
+      console.log("New Exercise Added!");
+      res.send(newExercise);
+    })
+    .catch((err) => {
+      console.log("Error saving exercise");
       console.log(err);
     });
 });
